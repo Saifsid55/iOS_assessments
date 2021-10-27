ae325996d8536e1dc4e7db89d34e6aca4085bbed
Saifsid55 <saifmohd003@gmail.com>
2021-10-28 02:54:26 +0530

    added

---
+++ AppLocalization/AppLocalization/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  AppLocalization
+//
+//  Created by Mohd Saif on 28/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mynameLbl: UILabel!
+    @IBOutlet weak var stateLbl: UILabel!
+    @IBOutlet weak var mystateLbl: UILabel!
+    @IBOutlet weak var countryLbl: UILabel!
+    @IBOutlet weak var mycountryLbl: UILabel!
+    @IBOutlet weak var dobLbl: UILabel!
+    @IBOutlet weak var mydobLbl: UILabel!
+    @IBOutlet weak var lotterywonLbl: UILabel!
+    @IBOutlet weak var amountLbl: UILabel!
+    
+    @IBOutlet weak var demoSeg: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func segAction(_ sender: UISegmentedControl) {
+        
+        if demoSeg.selectedSegmentIndex == 0
+        {
+            self.changingLanguage(lan: "en")
+        }
+        else if demoSeg.selectedSegmentIndex == 1
+        {
+            self.changingLanguage(lan: "de")
+        }
+        else
+        {
+            self.changingLanguage(lan: "es")
+        }
+    }
+    
+    func changingLanguage(lan : String)
+    {
+        nameLbl.text = "namekey".stringLoc(loc: lan)
+        mynameLbl.text = "mynamekey".stringLoc(loc: lan)
+        stateLbl.text = "statekey".stringLoc(loc: lan)
+        mystateLbl.text = "mystatekey".stringLoc(loc: lan)
+        countryLbl.text = "countrykey".stringLoc(loc: lan)
+        mycountryLbl.text = "mycountrykey".stringLoc(loc: lan)
+        dobLbl.text = "dobkey".stringLoc(loc: lan)
+        mydobLbl.text = "mydobkey".stringLoc(loc: lan)
+        lotterywonLbl.text = "lotterykey".stringLoc(loc: lan)
+        amountLbl.text = "amountkey".stringLoc(loc: lan)
+        
+    }
+    
+}
+
+extension String{
+    
+    func stringLoc(loc : String) -> String
+    {
+        let path = Bundle.main.path(forResource: loc, ofType: "lproj")
+        let bundle = Bundle(path: path!)
+        return NSLocalizedString(self, tableName: nil, bundle: bundle! , value: "", comment: "")
+        
+    }
+}
+
