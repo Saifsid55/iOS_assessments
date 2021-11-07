87fb47fe98c67853a316dbdb63e9e6da03575a56
Saifsid55 <saifmohd003@gmail.com>
2021-11-08 00:57:58 +0530

    Added

---
+++ ProtocolDelegation/ProtocolDelegation/Model/Profile.swift
@@ -0,0 +1,15 @@
+//
+//  Profile.swift
+//  ProtocolDelegation
+//
+//  Created by Mohd Saif on 02/11/21.
+//
+
+import Foundation
+
+
+struct Profile {
+    let name : String
+    let age : String
+    let pName : String
+}
