0d57d7a4445faa1c134b7f9c6c07ada6dee224bc
Saifsid55 <saifmohd003@gmail.com>
2021-11-08 01:31:54 +0530

    Added

---
+++ UnitTesting/UnitTesting/Model/ViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  ViewModel.swift
+//  UnitTesting
+//
+//  Created by Mohd Saif on 08/11/21.
+//
+
+import Foundation
+
+
+struct ViewModel {
+    
+    func validate(email : String, pass : String) -> (msg : String, isValid : Bool){
+        
+        guard !email.isEmpty && !pass.isEmpty else {
+            return ("Email or Password cannot be empty", false)
+        }
+        
+        guard isValidEmail(email) else {
+            return ("Email is invalid",false)
+        }
+        
+        guard pass.count > 6 else {
+            return ("Password should be greater than 6 characters",false)
+        }
+        
+        return ("Success",true)
+    }
+    
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+    
+}
